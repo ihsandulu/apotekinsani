45948595faad0e30d37fbe02889bbbba16519a33
ihsandulu <ihsan.dulu@gmail.com>
2025-07-24 12:24:07 +0700

    pos transaksi dikecil kecilin

--- app/Views/transaction/transaction_v.php
+++ app/Views/transaction/transaction_v.php
@@ -169,8 +169,10 @@
 
                         ?>
                         <form class="form col-12 " >
-                            <input onchange="listnota(-1)" type="date" id="from" name="from" class=" w-date" value="<?=$from;?>" style="width: 70px;font-size: 10px;">&nbsp;
-                            <input onchange="listnota(-1)" type="date" id="to" name="to" class=" w-date" value="<?=$to;?>" style="width: 70px;font-size: 10px;">&nbsp;&nbsp;
+                            <label for="from">Dari:</label>&nbsp;
+                            <input onchange="listnota(-1)" type="date" id="from" name="from" class=" w-date" value="<?=$from;?>" style="width: 50px;font-size: 10px;">&nbsp;
+                            <label for="to">Ke:</label>&nbsp;
+                            <input onchange="listnota(-1)" type="date" id="to" name="to" class=" w-date" value="<?=$to;?>" style="width: 50px;font-size: 10px;">&nbsp;&nbsp;
                             <button data-toggle="tooltip" data-placement="top" title="Hari Ini" onclick="hariini()" type="button" class="btn btn-primary btn-xs fa fa-calendar"></button>
                         </form>
                         <div class="my-1" id="listnota" style="display: block;"></div>  
