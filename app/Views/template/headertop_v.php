0c0281d0f70fce8d8b3ec5704d5764db44b890d9
ihsandulu <ihsan.dulu@gmail.com>
2025-07-24 12:39:39 +0700

    pos transaksi dikecil kecilin

--- app/Views/template/headertop_v.php
+++ app/Views/template/headertop_v.php
@@ -22,13 +22,13 @@
             <!-- toggle and nav items -->
             <ul class="navbar-nav mr-auto mt-md-0">
                 <!-- This is  -->
-                <!-- <li class="nav-item"> 
+                <li class="nav-item"> 
                     <a class="nav-link nav-toggler hidden-md-up text-muted" href="javascript:void(0);">
                         <i class="mdi mdi-menu" onclick="bukasidebar();"></i>
                     </a> 
-                </li> -->
+                </li>
                 <li class="nav-item m-l-10"> 
-                    <a class="nav-link sidebartoggler hidden-sm-down text-muted"  href="javascript:void(0);">
+                    <a class="nav-link sidebartoggler hidden-sm-down text-muted" onclick="bukasidebar();" href="javascript:void(0);">
                         <i class="ti-menu"></i>
                     </a> 
                 </li>
